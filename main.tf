9c7084adea5f598215c62a31e510ae47bcff4f54
ehazak <ehazak@gmail.com>
2023-03-29 12:34:12 -0400

    updated credentials?

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  /*access_key = var.aws_access_key
-  aws_secret = var.aws_secret*/
+  access_key = var.aws_access_key
+  aws_secret = var.aws_secret
 }
 
 resource "aws_vpc" "playgroud-vpc" {

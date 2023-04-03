12662f2162aca9186a745b7ed274e59712caeca3
ehazak <ehazak@gmail.com>
2023-04-03 13:19:58 -0400

    updates

--- main.tf
+++ main.tf
@@ -40,7 +40,8 @@ resource "aws_instance" "playground-vm" {
   ami = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
 
-  tags = {
-     Name = "playground-vm"
+  network_interface {
+    network_interface_id = aws_subnet.subnet-1.id
+    device_index = 0
   }
 }

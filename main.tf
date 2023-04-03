b38a8783e8469781e8fb0b1520e4548eefa99fc0
ehazak <ehazak@gmail.com>
2023-04-03 11:12:51 -0400

    Recreate VPC

--- main.tf
+++ main.tf
@@ -19,11 +19,10 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-/*
+
 resource "aws_vpc" "playgroud-vpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
         Name = "playground-vpc"
     }
 }
-*/
